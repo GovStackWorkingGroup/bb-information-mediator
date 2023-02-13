055771a3ae832014d2b3ae13c5ee7986a6249a37
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-02-13 16:22:49 +0100

    TECH-188: Changed used schema to test allowedMethods response to the one from helpers file

--- test/features/support/allowed_methods.js
+++ test/features/support/allowed_methods.js
@@ -1,70 +1,10 @@
 const { spec } = require('pactum');
 const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
-const { localhost } = require('./helpers/helpers');
+const { localhost, responseSchema } = require('./helpers/helpers');
 
 let specAllowedMethods;
 
 const baseUrl = `${localhost}{GovStackInstance}/{memberClass}/{memberCode}/{applicationCode}/allowedMethods`;
-
-const responseSchema = {
-  type: 'object',
-  properties: {
-    member: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          objectType: {
-            type: 'object',
-            properties: {
-              object_type: {
-                type: 'string',
-                enum: [
-                  'MEMBER',
-                  'SUBSYSTEM',
-                  'SERVER',
-                  'GLOBALGROUP',
-                  'SECURITYCATEGORY',
-                  'SERVICE',
-                  'CENTRALSERVICE',
-                  'LOCALGROUP',
-                ],
-              },
-            },
-            additionalProperties: false,
-          },
-          serviceType: { type: 'string' },
-          GovStackInstance: { type: 'string' },
-          memberClass: { type: 'string' },
-          memberCode: { type: 'string' },
-          applicationCode: { type: 'string' },
-          serviceCode: { type: 'string' },
-          serviceVersion: { type: 'string' },
-          endpointList: {
-            type: 'object',
-            properties: {
-              member: {
-                type: 'array',
-                items: {
-                  type: 'object',
-                  properties: {
-                    method: { type: 'string' },
-                    path: { type: 'string' },
-                  },
-                  additionalProperties: false,
-                },
-              },
-            },
-            additionalProperties: false,
-          },
-        },
-        additionalProperties: false,
-      },
-    },
-  },
-  required: ['member'],
-  additionalProperties: false,
-};
 
 Before(() => {
   specAllowedMethods = spec().expectResponseTime(15000);
